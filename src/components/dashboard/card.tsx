f2179df3bbb2941d0f75d74fcff34fbf21c5c6b2
Abd Fawwaz <abo.zanih1@gmail.com>
2023-04-03 15:41:23 +0300

    responsive layout

---
+++ src/components/dashboard/card.tsx
@@ -0,0 +1,115 @@
+import * as React from 'react';
+import styles from "@/styles/Card.module.scss"
+import { Card, Button, Space, Row, Col, Typography, Divider, Grid, Dropdown } from 'antd';
+import {MoreOutlined, DeleteOutlined, EditFilled} from "@ant-design/icons"
+import type { MenuProps } from 'antd';
+
+const {useBreakpoint} = Grid
+
+const DropdownItem: MenuProps["items"] = [
+    {
+        key:"1",
+        label:(
+            <Space size={39}>
+                    <Button icon={<MoreOutlined />}/>
+                    <Button danger icon={<DeleteOutlined />}/>
+            </Space>
+        ),
+    },
+    {
+        key:"2",
+        label:<Button type={"primary"}>Request Job</Button>,
+    }
+]
+const JobCard = () => {
+    const {md,xl,xs} = useBreakpoint()
+    const extras = (xs?
+                    <Dropdown menu={{ items:DropdownItem }} placement={"bottomRight"} trigger={['click']} arrow={{ pointAtCenter: true }}>
+                        <Button icon={<MoreOutlined />}/>
+                    </Dropdown>
+                    :
+                    <Space>
+                        <Button type={"primary"}>Request Job</Button>
+                        <Button icon={<MoreOutlined />}/>
+                        <Button icon={<EditFilled />}/>
+                        <Button danger icon={<DeleteOutlined />}/>
+                    </Space>)
+
+
+    return ( 
+        <Card title={<><span style={{fontWeight:"normal"}}>#1:</span> Job Title</> } extra={extras}>
+            <Row>
+                <Col md={24} xl={8}>
+                        <Row gutter={[0,((!xl && md)|| xs)?15:10]}>
+                            <Col xs={24} md={8} xl={24}>
+                                <Space size={0} direction={"vertical"}>
+                                    <Typography.Text strong>Requsutuin Type</Typography.Text>
+                                    <Typography.Text>new Vacancy</Typography.Text>
+                                </Space>
+                            </Col>
+                            <Col xs={24} md={8} xl={24}>
+                            <Space size={0} direction={"vertical"}>
+                                    <Typography.Text strong className={styles.primaryColor}>Organization Structure</Typography.Text>
+                                    <Typography.Text>Sadeq org</Typography.Text>
+                                </Space>
+                            </Col>
+                            <Col xs={24} md={8} xl={24}>
+                            <Space size={0} direction={"vertical"}>
+                                    <Typography.Text strong>Units</Typography.Text>
+                                    <Typography.Text>unit 1, unit  2</Typography.Text>
+                                </Space>
+                            </Col>
+                            {xl && <Divider className={styles.my1} />} 
+                            <Col span={24}>
+                                <Space direction={"vertical"}>
+                                    <Typography.Text strong>Hiring manager</Typography.Text>
+                                    <Space>
+                                        <Typography.Text className={`${styles.bgSuccess} ${styles.hiringManagers}`} >Hiring manager</Typography.Text>
+                                        <Typography.Text className={`${styles.bgSuccess} ${styles.hiringManagers}`} >Hiring manager</Typography.Text>
+                                    </Space>
+                                </Space>
+                            </Col>
+                        </Row>
+                       {((md&&!xl) || xs) && <Divider className={styles.my1} />} 
+                </Col>
+                <Col xs={24} md={24} xl={16}>
+                    <Row gutter={[0,25]}>
+                        <Col style={{textAlign:((!xl && md)|| xs)?"start":"end"}} span={24}>
+                            <Space size={0} direction={"vertical"}>
+                                <Typography.Text>Current Requisition Status</Typography.Text>
+                                <Typography.Text type={"danger"}>Not Requested</Typography.Text>
+                                <Typography.Link>View Details</Typography.Link>
+                            </Space>
+                        </Col>
+                        <Col span={24}>
+                            <Row justify={((!xl && md)|| xs)?"start":"end"} gutter={[0,25]}>
+                                <Col xs={24} md={7} lg={8} xl={7}>
+                                    <Card bordered={false} style={{height:"100%"}} size={"small"} className={styles.card}>
+                                        <Typography.Text strong>1</Typography.Text>
+                                        <Typography.Paragraph>Total Budgeted Vacancies</Typography.Paragraph>
+                            
+                                    </Card>
+                                </Col>
+                                <Col xs={24} md={7} lg={8} xl={7}>
+                                    <Card bordered={false} style={{height:"100%"}} size={"small"} className={styles.card}>
+                                        <Typography.Text type={"success"} strong>1</Typography.Text>
+                                        <Typography.Paragraph>Vacancies still open</Typography.Paragraph>
+                                    </Card>
+                                </Col>
+                                <Col xs={24} md={7} lg={8} xl={7}>
+                                    <Card bordered={false} style={{height:"100%"}} size={"small"} className={styles.card}>
+                                    <Typography.Text type={"danger"} strong>0</Typography.Text>
+                                    <Typography.Paragraph>Vacancies Field</Typography.Paragraph>
+                                        
+                                    </Card>
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+        </Card>
+     );
+}
+ 
+export default JobCard;
--- src/components/dashboard/index.tsx
+++ src/components/dashboard/index.tsx
@@ -1,11 +1,35 @@
 import Sidebar from "./sidebar";
-import {Row, Col} from "antd";
-const Dashboard = () => {
+import {Row, Col, Space, Input, Button, Grid} from "antd";
+import {SearchOutlined, ReloadOutlined,FilterOutlined, DownloadOutlined, UploadOutlined, PlusCircleFilled} from "@ant-design/icons"
+import styles from "@/styles/dashboard.module.scss"
+import JobCard from "./card";
 
+const {useBreakpoint} = Grid
+const Dashboard = () => {
+    const {xs,xl} = useBreakpoint()
     return (
-        <Row>
-            <Col>
-                h1
+        <Row gutter={[0,10]} className={`${styles.mt1} ${styles.mx1}`}>
+            <Col span={24}>
+                <Row justify={"space-between"}>
+                    <Col>
+                        <Space>
+                            <Input size={xs?"middle":"large"} placeholder="Search" />
+                            <Button size={xs?"middle":"large"} className={`${styles.dFlex} ${styles.justifyContentCenter} ${styles.alignItemsCenter}`}  type="primary" icon={<SearchOutlined style={{fontSize:"10px"}} />} />
+                            <Button size={xs?"middle":"large"} className={`${styles.dFlex} ${styles.justifyContentCenter} ${styles.alignItemsCenter} ${styles.secoundryButton}`} icon={<ReloadOutlined style={{fontSize:"10px"}} />} />
+                            <Button size={xs?"middle":"large"} className={`${styles.ml1} ${styles.dFlex} ${styles.justifyContentCenter} ${styles.alignItemsCenter} ${styles.secoundryButton}`} icon={<FilterOutlined />}>{xl?<span>Fillter</span>:""}</Button>
+                        </Space>
+                    </Col>
+                    <Col>
+                        <Space>
+                            <Button size={xs?"middle":"large"} className={`${styles.dFlex} ${styles.justifyContentCenter} ${styles.alignItemsCenter} ${styles.secoundryButton}`} icon={<DownloadOutlined style={{fontSize:"10px"}} />}>{xl?"Download Template":""}</Button>
+                            <Button size={xs?"middle":"large"} className={`${styles.dFlex} ${styles.justifyContentCenter} ${styles.alignItemsCenter} ${styles.secoundryButton}`} icon={<UploadOutlined style={{fontSize:"10px"}} />}>{xl?"Import Jobs":""}</Button>
+                            <Button size={xs?"middle":"large"} type="primary"  className={`${styles.dFlex} ${styles.justifyContentCenter} ${styles.alignItemsCenter}`} icon={<PlusCircleFilled />}>{xl?"Add new Job":""}</Button>
+                        </Space>
+                    </Col>
+                </Row>
+            </Col>
+            <Col span={24}>
+                <JobCard/>
             </Col>
         </Row>
     );
